d477ff55705f8caba8377f8e90ce6839827f93f8
danisqxas <mcdw.danizt@gmail.com>
2025-05-09 01:08:30 -0300

    🚀 Estructura mínima corregida y lista para Vercel

--- api/chat_summarizer.py
+++ api/chat_summarizer.py
@@ -23,6 +23,6 @@ class handler(BaseHTTPRequestHandler):
         }
 
         self.send_response(200)
-        self.send_header('Content-type', 'application/json')
+        self.send_header("Content-Type", "application/json")
         self.end_headers()
         self.wfile.write(json.dumps(reply).encode())
